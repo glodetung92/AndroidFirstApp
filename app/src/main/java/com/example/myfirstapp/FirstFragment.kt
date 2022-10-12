bb1796f7865219e2225a0db777f6631fad5a66ef
tunghao1802 <glodetung92@gmail.com>
2022-10-12 12:06:55 +0900

    complete Task8

--- app/src/main/java/com/example/myfirstapp/FirstFragment.kt
+++ app/src/main/java/com/example/myfirstapp/FirstFragment.kt
@@ -1,10 +1,14 @@
 package com.example.myfirstapp
 
+import android.os.Build.VERSION_CODES.S
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.myfirstapp.databinding.FragmentFirstBinding
 
@@ -12,6 +16,18 @@ import com.example.myfirstapp.databinding.FragmentFirstBinding
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
+
+    private fun countMe (view: View) {
+        // Get the text view
+        var showCountTextView = view.findViewById<TextView>(R.id.textview_first)
+        // Get the value of the text view
+        val countString = showCountTextView.text.toString()
+        // Convert value to a number and increment it
+        var count = countString.toInt()
+        count++
+        // Display the new value in the text view
+        showCountTextView.text = count.toString()
+    }
 
     private var _binding: FragmentFirstBinding? = null
 
@@ -31,9 +47,18 @@ class FirstFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        binding.buttonFirst.setOnClickListener {
+        // Random Button
+        view.findViewById<Button>(R.id.random_button).setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+        // Toast Button
+        view.findViewById<Button>(R.id.toast_button).setOnClickListener {
+            val myToast = Toast.makeText(context, "Hello Toast!!!", Toast.LENGTH_SHORT)
+            myToast.show()
+        }
+        // Count Button
+        view.findViewById<Button>(R.id.count_button).setOnClickListener {
+            countMe(view)
         }
     }
 
